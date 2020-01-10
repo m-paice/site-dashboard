cf3674cf8be7aff770522aeecfbcaa171386d043
Matheus Paice <matheuspaice@mandeumzap.com.br>
2020-01-10 14:03:46 -0300

    add remote dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import createSagaMiddleware from "redux-saga";
 
@@ -9,7 +10,7 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducers,
-  applyMiddleware(sagaMiddleware, logger)
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
 
 sagaMiddleware.run(rootSagas);
